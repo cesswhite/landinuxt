66ab24984e41e04a0787463454a363af41facd48
Cess White <acessloop@gmail.com>
2024-10-16 19:35:30 -0600

    [ADD] - Logo component

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="fixed left-0 top-0 z-50 w-full bg-gray-50/70 dark:bg-gray-950/70 backdrop-blur-md">
     <div class="flex items-center justify-between px-4 py-2">
-      <div class="flex size-12 items-center justify-center">
-        <NuxtLink to="/"> Logo </NuxtLink>
+      <div class="flex size-8 items-center justify-center">
+        <NuxtLink to="/">
+          <AppLogo />
+        </NuxtLink>
       </div>
       <nav class="flex items-center justify-center">
         <AppSwitchPrimaryColor />
@@ -12,4 +14,5 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
