df532d2fc05f81afe2c79f97bb11531b1a03cba5
Cess White <acessloop@gmail.com>
2024-11-04 09:32:25 -0600

    [ADD] - SEO Details

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative w-full min-h-dvh h-auto bg-dark-50 dark:bg-dark-900">
+    <div class="relative w-full min-h-dvh h-auto bg-dark-50 dark:bg-dark-950">
         <AppHeader />
         <slot />
         <AppFooter />
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,14 @@
 
 <script setup>
 useSeoMeta({
-  title: "LandiNuxt | Build fast Landing Pages with Nuxt Ecosystem",
+  title: "LandiNuxt | Effortless Landing Page Creation",
+  ogTitle: "LandiNuxt | Effortless Landing Page Creation",
+  description:
+    "Build beautiful, responsive landing pages effortlessly with LandiNuxt. Access a curated library of pre-built components, streamline your design workflow, and deploy faster.",
+  ogDescription:
+    "Create stunning landing pages with ease using LandiNuxt's pre-built components library. Perfect for developers looking to accelerate their projects without compromising on quality.",
+  ogImage: "/og-landinuxt.jpg",
+  twitterCard: "summary_large_image",
 });
+
 </script>
