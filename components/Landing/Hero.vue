87fd1df5cbf4478953d6ad0b0ec115daba668e90
Cess White <acessloop@gmail.com>
2024-12-21 17:21:10 -0600

    [ADD] - Improve button text in hero section

--- components/Landing/Hero.vue
+++ components/Landing/Hero.vue
@@ -55,7 +55,7 @@
         <div class="mt-12 flex flex-col items-center justify-center gap-4 md:flex-row">
           <UButton to="/instructions" variant="ghost" size="xl"> Instructions </UButton>
           <UButton variant="solid" to="/components" size="xl">
-            Discover 50+ Components
+            Explore 50+ Free Components Now
           </UButton>
         </div>
       </div>
