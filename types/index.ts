8269eee100cf58364b8f81b83daca7ca4ac1e18e
Cess White <acessloop@gmail.com>
2025-05-07 18:42:46 -0600

    [REMOVE] - Deleted commented-out repository data from Container.vue and cleaned up Repository interface in types/index.ts

--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,13 @@
 export type Elements = "hero" | "headers" | "features" | "footers" | "cta" | 'testimonials' | 'contact' | 'logos' | 'faq' | 'auth';
 export type LandingPages = 'simple' | 'showcase' | 'saas' | 'agency'
-/* export type Repositories = 'v4.20' | 'nuxoki' | 'nuxt-clerk' | 'hato' | 'lotus' */
-
-
 export interface Component {
   name: Elements;
   total: number;
 }
-
 export interface Landing {
   name: LandingPages;
   image: string
 }
-
 export interface Repository {
   name: string
   url: string
@@ -20,14 +15,6 @@ export interface Repository {
   image: string
   isPremium: boolean
 }
-
-/* export interface Repository {
-  name: Repositories;
-  image: string | (() => Promise<string>);
-  url: string
-  description: string
-} */
-
 declare global {
   interface Window {
     umami: {
