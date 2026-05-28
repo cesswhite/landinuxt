d7267b07fab907db2df3d84419c98af2d205a996
Cess White <acessloop@gmail.com>
2026-05-27 21:09:33 -0600

    [UPDATE] - Adjust layout properties in Container and Tile components for improved alignment and responsiveness. Update grid and flex properties to enhance visual consistency across the application.

--- app/components/Elements/Tile.vue
+++ app/components/Elements/Tile.vue
@@ -1,10 +1,10 @@
 <template>
   <NuxtLink
     :to="`/components/${props.data.name}`"
-    class="col-span-full h-auto transform-gpu cursor-pointer rounded-xl transition-transform duration-300 ease-in-out will-change-transform sm:col-span-6 md:col-span-4 2xl:col-span-3 bg-dark-50 p-1 ring-1 ring-dark-950/10 shadow shadow-dark-950/10 dark:bg-dark-950 dark:ring-dark-50/15 dark:shadow-2xs dark:shadow-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
+    class="col-span-full h-auto self-start transform-gpu cursor-pointer rounded-xl transition-transform duration-300 ease-in-out will-change-transform sm:col-span-6 md:col-span-4 2xl:col-span-3 bg-dark-50 p-1 ring-1 ring-dark-950/10 shadow shadow-dark-950/10 dark:bg-dark-950 dark:ring-dark-50/15 dark:shadow-2xs dark:shadow-black focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-500"
     @click="onCategoryClick"
   >
-    <div class="aspect-1080/670 w-full overflow-hidden rounded-lg bg-dark-50 dark:bg-dark-950">
+    <div class="aspect-1080/670 w-full shrink-0 overflow-hidden rounded-lg bg-dark-50 dark:bg-dark-950">
       <component :is="coverComponent" />
     </div>
     <div class="mt-2 p-1 text-base font-bold capitalize text-dark-950/90 dark:text-dark-50/90">
